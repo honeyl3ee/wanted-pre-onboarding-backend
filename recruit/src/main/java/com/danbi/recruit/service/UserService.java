0819a67b78bcc44ec3945ed7fd273643d985ab2f
Danbi <ddanhopark@gmail.com>
2023-10-14 16:36:01 +0900

    Feat: Add find user
    유저 조회 기능 추가함

--- recruit/src/main/java/com/danbi/recruit/service/UserService.java
+++ recruit/src/main/java/com/danbi/recruit/service/UserService.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -17,5 +19,13 @@ public class UserService {
 
     public void join(Users user) {
         userRepository.save(user);
+    }
+
+    public Users findUser(Long userId) {
+        return userRepository.findOne(userId);
+    }
+
+    public List<Users> findAllUsers() {
+        return userRepository.findAll();
     }
 }
