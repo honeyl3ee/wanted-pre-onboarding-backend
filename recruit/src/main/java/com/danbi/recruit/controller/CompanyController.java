c64eb8f1e43640e7d3e9e1ba0e3a51a3040da800
Danbi <ddanhopark@gmail.com>
2023-10-14 19:38:45 +0900

    Init: Add Company Controller
    회사 저장 컨트롤러를 작성함

--- recruit/src/main/java/com/danbi/recruit/controller/CompanyController.java
+++ recruit/src/main/java/com/danbi/recruit/controller/CompanyController.java
@@ -1,2 +1,52 @@
-package com.danbi.recruit.controller;public class CompanyController {
+package com.danbi.recruit.controller;
+
+import com.danbi.recruit.domain.Company;
+import com.danbi.recruit.repository.CompanyRepository;
+import com.danbi.recruit.service.CompanyService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CompanyController {
+
+    private final CompanyService companyService;
+
+    @PostMapping("/api/companies")
+    public CreateCompanyResponse saveCompany(@RequestBody @Valid CreateCompanyRequest request) {
+        Company company = new Company();
+        company.setName(request.name);
+        company.setCountry(request.country);
+        company.setCity(request.city);
+
+        Long companyId = companyService.join(company);
+
+        return new CreateCompanyResponse(companyId);
+    }
+
+    @Data
+    static class CreateCompanyRequest {
+        @NotEmpty
+        private String name;
+
+        @NotEmpty
+        private String country;
+
+        @NotEmpty
+        private String city;
+    }
+
+    @Data
+    static class CreateCompanyResponse {
+        private Long id;
+
+        public CreateCompanyResponse(Long id) {
+            this.id = id;
+        }
+    }
 }
