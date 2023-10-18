56227191f65f28f56a41a2ac995e71aab5f16614
Danbi <ddanhopark@gmail.com>
2023-10-18 20:51:22 +0900

    Feat: Add exception to findOne and searchRecruit method
    존재하지 않는 id 조회시 예외 발생, 키워드 검색 기능 추가

--- recruit/src/main/java/com/danbi/recruit/repository/RecruitRepository.java
+++ recruit/src/main/java/com/danbi/recruit/repository/RecruitRepository.java
@@ -3,7 +3,9 @@ package com.danbi.recruit.repository;
 import com.danbi.recruit.domain.Company;
 import com.danbi.recruit.domain.Recruit;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -24,7 +26,10 @@ public class RecruitRepository {
     }
 
     public Recruit findOne(Long recruitId) {
-        return em.find(Recruit.class, recruitId);
+        Recruit findRecruit = em.find(Recruit.class, recruitId);
+        if (findRecruit == null)
+            throw new InvalidDataAccessApiUsageException("존재하지 않는 공고 id 입니다.");
+        return findRecruit;
     }
 
     public List<Recruit> findByCompany(Long companyId) {
@@ -34,6 +39,33 @@ public class RecruitRepository {
                 .getResultList();
     }
 
-//    public Optional<List<Recruit>> searchRecruit()
+    public void delete(Recruit recruit) {
+        em.remove(recruit);
+    }
+
+    public Optional<List<Recruit>> searchRecruit(String keyword) {
+        String jpqlQuery = "select r from Recruit r " +
+                "where r.position like :keyword " +
+                "or r.techStack like :keyword " +
+                "or r.company.name like :keyword " +
+                "or r.company.country like :keyword " +
+                "or r.company.city like :keyword";
+
+        TypedQuery<Recruit> query = em.createQuery(jpqlQuery, Recruit.class);
+        query.setParameter("keyword", "%" + keyword + "%");
+
+        List<Recruit> results = query.getResultList();
+
+        return Optional.ofNullable(results);
+    }
+
+    //public Optional<List<Recruit>> searchRecruit() {
+//        return em.createQuery("select o from Order o join o.member m" + " where o.status = :status" +
+//        " and m.name like :name", Orders.class)
+//                .setParameter("status", orderSearch.getOrderStatus())
+//                .setParameter("name", orderSearch.getMemberName())
+//                .setMaxResults(1000)
+//                .getResultList();
+//    }
 
 }
