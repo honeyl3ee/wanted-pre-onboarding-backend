f7e77bd5ed7f2078a66c160eea1716042ea37e5d
Danbi <ddanhopark@gmail.com>
2023-10-18 21:06:09 +0900

    Init: Add recruit controller
    공고 게시, 수정, 조회, 목록 조회, 삭제, 검색, 지원 컨트롤러 추가

--- recruit/src/main/java/com/danbi/recruit/controller/RecruitController.java
+++ recruit/src/main/java/com/danbi/recruit/controller/RecruitController.java
@@ -1,20 +1,26 @@
 package com.danbi.recruit.controller;
 
+import com.danbi.recruit.DTO.RecruitDTO;
 import com.danbi.recruit.domain.Company;
 import com.danbi.recruit.domain.Recruit;
 import com.danbi.recruit.domain.Users;
 import com.danbi.recruit.service.CompanyService;
 import com.danbi.recruit.service.RecruitService;
+import com.danbi.recruit.service.UserService;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -23,42 +29,68 @@ public class RecruitController {
 
     private final RecruitService recruitService;
     private final CompanyService companyService;
+    private final UserService userService;
 
     @PostMapping("/api/recruits")
-    public CreateRecruitResponse postRecruit(@RequestBody @Valid CreateRecruitRequest request) {
+    public RecruitDTO.CreateRecruitResponse postRecruit(@RequestBody @Valid RecruitDTO.CreateRecruitRequest request) {
 
-        Company company = companyService.findOne(request.companyId);
-        Recruit recruit = Recruit.createRecruit(company, request.reward, request.content, request.position, request.techStack);
+        Company company = companyService.findOne(request.getCompanyId());
+        Recruit recruit = Recruit.createRecruit(company, request.getReward(), request.getContent(), request.getPosition(), request.getTechStack());
         Long recruitId = recruitService.post(recruit);
 
-        return new CreateRecruitResponse(recruitId);
-
-
-    }
-    @Data
-    static class CreateRecruitRequest {
-        @NotEmpty
-        private Long companyId;
-
-        @NotEmpty
-        private String position;
-
-        @NotEmpty
-        private Long reward;
-
-        @NotEmpty
-        private String techStack;
-
-        @NotEmpty
-        private String content;
+        return new RecruitDTO.CreateRecruitResponse(recruitId);
     }
 
-    @Data
-    static class CreateRecruitResponse {
-        private Long id;
+    @PutMapping("api/recruits/{id}")
+    public RecruitDTO.UpdateRecruitResponse updateRecruit(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid RecruitDTO.UpdateRecruitRequest request) {
+        Long recruitId = recruitService.update(id, request);
+        return new RecruitDTO.UpdateRecruitResponse(recruitService.findRecruit(recruitId));
+    }
 
-        public CreateRecruitResponse(Long id) {
-            this.id = id;
-        }
+    @GetMapping("/api/recruits")
+    public List<RecruitDTO.ReadRecruit> findAllRecruits() {
+        List<Recruit> findRecruits = recruitService.findRecruits();
+        List<RecruitDTO.ReadRecruit> collect = findRecruits.stream()
+                .map(m -> new RecruitDTO.ReadRecruit(m.getId() ,m.getCompany().getName(), m.getCompany().getCountry(), m.getCompany().getCity(), m.getPosition(), m.getReward(), m.getTechStack()))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @GetMapping("api/recruits/{id}")
+    public RecruitDTO.ReadRecruit findRecruit(@PathVariable("id") Long id) {
+        Recruit recruit = recruitService.findRecruit(id);
+        return new RecruitDTO.ReadRecruit(recruit);
+    }
+
+    @GetMapping("api/recruits/{id}/detail")
+    public RecruitDTO.ReadDetailedRecruit findDetailedRecruit(@PathVariable("id") Long id) {
+        Recruit recruit = recruitService.findRecruit(id);
+        return new RecruitDTO.ReadDetailedRecruit(recruit);
+    }
+
+    @GetMapping("api/recruits/search")
+    public Optional<List<RecruitDTO.ReadRecruit>> searchRecruit(@RequestParam String search) {
+        Optional<List<Recruit>> recruits = recruitService.searchRecruit(search);
+        return Optional.of(recruits.orElse(Collections.emptyList())
+                .stream()
+                .map(m -> new RecruitDTO.ReadRecruit(m.getId(), m.getCompany().getName(), m.getCompany().getCountry(), m.getCompany().getCity(), m.getPosition(), m.getReward(), m.getTechStack()))
+                .toList());
+    }
+
+    @DeleteMapping("api/recruits/{id}")
+    public RecruitDTO.DeleteRecruitResponse deleteRecruit(@PathVariable("id") Long id) {
+        Recruit recruit = recruitService.findRecruit(id);
+        recruitService.delete(id);
+        return new RecruitDTO.DeleteRecruitResponse(id);
+    }
+
+
+    @PatchMapping("api/recruits/{id}/apply")
+    public RecruitDTO.ApplyResponse applyRecruit(@PathVariable("id") Long id, @RequestBody @Valid RecruitDTO.ApplyRequest request) {
+        Users user = userService.findUser(request.getUserId());
+
+        return new RecruitDTO.ApplyResponse(user, recruitService.apply(id, user.getId()));
     }
 }
